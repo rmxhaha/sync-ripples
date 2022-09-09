26b88d39b0716b6d1122f98bd9aaae933c527774
rmxhaha <code000123@gmail.com>
2022-09-09 16:48:58 +0800

    sync ripples v1!!!

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+var connectCounter = 0;
+
+io.on('connect', function(socket) {
+    connectCounter++; 
+    console.log('a user connected');
+
+    socket.on('click', (event) => {
+        io.emit('click', event);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      connectCounter--;
+    });
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+setInterval(() => {
+    console.log(`${connectCounter} users connected`);
+}, 5000);
